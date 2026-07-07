ff8cb18a567d938c299870945fc38b1548e6d64b
Nagaraj-goudar <nagarajgoudar9686@gmail.com>
2026-07-07 16:50:53 +0530

    Convert Vehicle into abstract base class

--- src/com/vehicle/Vehicle.java
+++ src/com/vehicle/Vehicle.java
@@ -1,6 +1,26 @@
 package com.vehicle;
 
-public class Vehicle {
-	String name;
-	String model;
+public abstract class Vehicle {
+
+    String brand;
+    int speed;
+    String engineNumber;
+    int fuelCapacity;
+
+    Vehicle(String brand, int speed, String engineNumber, int fuelCapacity) {
+        this.brand = brand;
+        this.speed = speed;
+        this.engineNumber = engineNumber;
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    void start() {
+        System.out.println(brand + " Started");
+    }
+
+    void stop() {
+        System.out.println(brand + " Stopped");
+    }
+
+    abstract void drive();
 }
